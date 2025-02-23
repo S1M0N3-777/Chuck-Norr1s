4d766762a1f3c5c2168aef93abca64b61b7c9ac3
S1M0N3-777 <simoalbanese07@gmail.com>
2025-02-23 12:08:50 +0100

    classList non funzionante

--- static/js/script.js
+++ static/js/script.js
@@ -1,25 +1,44 @@
 let btn = document.querySelector("#start")
 let inner = document.querySelector("#inner")
 let url = ""
+let immagine = document.querySelector("#image")
+console.log(immagine)
+let classe = "image"
+let rnd = random(1,3)
+rnd = rnd.toString()
+classe = classe + rnd
+console.log(classe)
+function random(min, max) {
+    let n = Math.random() * (max - min) + min
+    n = parseInt(n)
+    console.log(n)
+    return n
+                }
+
 btn.addEventListener("click", (e) => {
     e.preventDefault()
     let scelta = document.querySelector("#scelta").value.toLowerCase()
     console.log(scelta)
-    url = "https://api.chucknorris.io/jokes/random?category=" + scelta
-    console.log(url)
-    let p = fetch(url).then(
-        function (response) {
-            return response.json()
-        }
-    ).then(
-        function (data) {
-            console.log(data)
-            inner.innerHTML = data.value
-        }
-    ).catch(
-        function (error) {
-            console.log(error)
-        }
-    )
-
+    if(scelta !==  "none"){
+        url = "https://api.chucknorris.io/jokes/random?category=" + scelta
+        console.log(url)
+        let p = fetch(url).then(
+            function (response) {
+                return response.json()
+            }
+        ).then(
+            function (data) {
+                console.log(data)
+                inner.innerHTML = data.value
+                immagine.classList.add(classe)
+                immagine.classList.remove("image")
+            }
+        ).catch(
+            function (error) {
+                console.log(error)
+            }
+        )
+    }else{
+        inner.innerHTML = "Seleziona una categoria"
+    }
 })
